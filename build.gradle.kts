17286e63c482cc251ddc1ccd20c8172fd59d5cf4
guozhenkuang <guozhenkuang@genew.com>
2026-08-30 21:36:23 +0800

    build(core-net): test-retry 升级到 1.6.5
    1.6.2 在 Gradle 9.3.1 + JDK 25 的 CI 上偶发测试进程 NPE（测试报告本身
    全部通过、进程异常退出），升级到最新补丁版本。

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
     id("org.jetbrains.kotlinx.kover") version "0.9.8" apply false
-    id("org.gradle.test-retry") version "1.6.2" apply false
+    id("org.gradle.test-retry") version "1.6.5" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
 }
 
