1a9e7a0f848bd94d94d26ff0caa471cbbbadbf60
guozhenkuang <guozhenkuang@genew.com>
2026-08-30 17:05:14 +0800

    style(core-net): 修正 StateFlow 导入顺序

--- core-net/src/main/kotlin/top/guozk/pipilot/corenet/WebSocketTransport.kt
+++ core-net/src/main/kotlin/top/guozk/pipilot/corenet/WebSocketTransport.kt
@@ -10,8 +10,8 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
